5fd5d590492ed373281ab3651c37126ce7bc831d
OmarHumamah <omar.nabeel.h@gmail.com>
2021-08-31 16:49:23 +0300

    labe 03 is done

---
+++ src/components/SelectedBeast.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+class SelectedBeast extends React.Component {
+
+
+    render() {
+        return (
+            <>
+
+                <Modal show={this.props.show}>
+                    <Modal.Header>{this.props.data.title}</Modal.Header>
+                    <Modal.Body>
+                        <Card.Img style={{ height: '18rem' }} variant="top" src={this.props.data.image_url} />
+                        <Card.Text>{this.props.data.description}</Card.Text>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={this.props.close} >close</Button>
+                    </Modal.Footer>
+
+                </Modal>
+            </>
+        )
+
+    }
+}
+
+export default SelectedBeast
