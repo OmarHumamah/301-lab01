8f9ab59b6f33db214c39e8e641ca7a993f504107
OmarHumamah <omar.nabeel.h@gmail.com>
2021-09-01 22:21:58 +0300

    ;labe 04

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,24 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
+import Row from "react-bootstrap/Row";
 
 class Main extends React.Component {
   render() {
     return (
       <>
-        {this.props.data.map((x) => {
-          return (
-            <HornedBeast
-              showModel={this.props.showModel}
-              data={x}
-              title={x.title}
-              img={x.image_url}
-              description={x.description}
-            />
-          );
-        })}
+        <Row xs={1} md={4}>
+          {this.props.fData.map((x) => {
+            return (
+              <HornedBeast
+                showModel={this.props.showModel}
+                data={x}
+                title={x.title}
+                img={x.image_url}
+                description={x.description}
+              />
+            );
+          })}
+        </Row>
       </>
     );
   }
